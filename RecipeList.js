1b88a84709f4a46e3429d68747f410dc4320f929
Evelyn <146141136+EvrenInk@users.noreply.github.com>
2024-02-28 18:40:50 -0800

    Create RecipeList.js

---
+++ RecipeList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import RecipeData from "./RecipeData";
+
+function RecipeList({ recipes, deleteRecipe }) {
+  
+  const handleDelete = (index) => {
+    deleteRecipe(index);
+  };
+
+  return (
+    <div className="recipe-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipes.map((recipe, index) => (
+            <tr key={index}>
+              <td>{recipe.name}</td>
+              <td>{recipe.cuisine}</td>
+              <td><img src={recipe.photo} alt={recipe.name} style={{ width: "100px", height: "auto" }} /></td>
+              <td className="content_td"><p>{recipe.ingredients}</p></td>
+              <td className="content_td"><p>{recipe.preparation}</p></td>
+              <td><button name="delete" onClick={() => handleDelete(index)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RecipeList;
