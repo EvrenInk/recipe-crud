ced14bd60d1a05b8f6e68b59167117b49a69a320
Evelyn <146141136+EvrenInk@users.noreply.github.com>
2024-02-28 18:39:45 -0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  const deleteRecipe = (index) => {
+    const updatedRecipes = [...recipes];
+    updatedRecipes.splice(index, 1);
+    setRecipes(updatedRecipes);
+  };
+
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipe={addRecipe} className="RecipeCreate" />
+    </div>
+  );
+}
+
+export default App;
